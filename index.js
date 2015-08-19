7a484175ac7a2af4809b6c5a943b37d3448df9fb
Tomasz Niezgoda <tomek.niezgoda@mail.com>
2015-08-19 19:07:22 +0200

    added more options

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-module.exports = function(prefix){
+var colors = require('colors/safe');
+
+module.exports = function(prefix, color){
+  var fullPrefix;
+  var messageReformat;
+
+  if(typeof prefix === 'undefined'){
+    fullPrefix = '';
+  }else{
+    fullPrefix = prefix + ': ';
+  }
+
+  if(typeof color === 'undefined'){
+    messageReformat = function(){};
+  }else{
+    messageReformat = function(message){return colors[color](messageReformat)};
+  }
 
   return {
     trace: function(message){
-      console.trace(prefix + ': ' + message);
+      console.trace(messageReformat(fullPrefix + message));
     },
     log: function(message){
-      console.log(prefix + ': ' + message);
+      console.log(messageReformat(fullPrefix + message));
     }
   };
 };
