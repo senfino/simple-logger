99727813beccea94ed817e079082a736d7c66d72
Tomasz Niezgoda <tomek.niezgoda@mail.com>
2015-08-20 15:04:28 +0200

    Ability to use custom options during single logger.

--- index.js
+++ index.js
@@ -1,31 +1,62 @@
 var colors = require('colors/safe');
+var _ = require('lodash');
 
-module.exports = function(prefix, color){
-  var fullPrefix;
-  var messageReformat;
+module.exports = function(prefix, color, noDefaults){
+  var defaultOptions;
 
-  if(typeof prefix === 'undefined'){
-    fullPrefix = '';
-  }else{
-    fullPrefix = prefix + ': ';
+  function getOptions(prefix, color){
+    var fullPrefix;
+    var messageReformat;
+
+    if(typeof prefix === 'undefined'){
+
+      if(noDefaults === true){
+        fullPrefix = undefined;
+      }else{
+        fullPrefix = '';
+      }
+    }else{
+      fullPrefix = prefix + ': ';
+    }
+
+    if(typeof color === 'undefined'){
+
+      if(noDefaults === true){
+        messageReformat = undefined;
+      }else{
+        messageReformat = function(message){
+          return message;
+        };
+      }
+    }else{
+      messageReformat = function(message){
+        return colors[color](message);
+      };
+    }
+
+    return {
+      fullPrefix: fullPrefix,
+      messageReformat: messageReformat
+    };
   }
 
-  if(typeof color === 'undefined'){
-    messageReformat = function(message){
-      return message;
-    };
-  }else{
-    messageReformat = function(message){
-      return colors[color](message);
-    };
-  }
+  defaultOptions = getOptions(prefix, color);
 
   return {
-    trace: function(message){
-      console.trace(messageReformat(fullPrefix + message));
+    trace: function(message, options){
+      var options = _.merge({}, defaultOptions, getOptions(options.prefix, options.color, true));
+
+      console.trace(options.messageReformat(options.fullPrefix + message));
     },
-    log: function(message){
-      console.log(messageReformat(fullPrefix + message));
+    log: function(message, options){
+      var options = _.merge({}, defaultOptions, getOptions(options.prefix, options.color, true));
+      
+      console.log(options.messageReformat(fullPrefix + message));
+    },
+    warn: function(message, options){
+      var options = _.merge({}, defaultOptions, getOptions(options.prefix, options.color, true));
+
+      console.warn(options.messageReformat(fullPrefix + message));
     }
   };
 };
